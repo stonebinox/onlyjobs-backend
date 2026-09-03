6905c7f0354292ac2f0cf4020b9b796e84bfc4cc
Anoop Santhanam <anoop.santhanam@gmail.com>
2026-09-03 11:08:29 +0530

    refactor: lean-safe _id identity in filterJobsForUser (onlyjobs-3tx.1)
    Match onlyjobs-background's filterJobsForUser (widened generic + job._id.
    toString()) so the copy-pasted util does not diverge. Behaviour-preserving
    for hydrated docs (job.id === job._id.toString()); no backend callers today.

--- src/utils/filterJobsForUser.ts
+++ src/utils/filterJobsForUser.ts
@@ -1,10 +1,9 @@
-import { IJobListing } from "../models/JobListing";
-import { IUser } from "../models/User";
+import { Types } from "mongoose";
 
-export const filterJobsForUser = (
-  jobs: IJobListing[],
-  user: IUser
-): IJobListing[] => {
+export const filterJobsForUser = <T extends { _id: { toString(): string } }>(
+  jobs: T[],
+  user: { skippedJobs: Types.ObjectId[] }
+): T[] => {
   const skippedJobsIds = user.skippedJobs.map((id) => id.toString());
-  return jobs.filter((job) => !skippedJobsIds.includes(job.id.toString()));
+  return jobs.filter((job) => !skippedJobsIds.includes(job._id.toString()));
 };
